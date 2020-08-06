6f00a5402199bb40dab2d75fd0fa00d85847e754
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2020-08-06 14:47:03 +0700

    register member

--- application/views/master/member/add.php
+++ application/views/master/member/add.php
@@ -1,62 +1,63 @@
 <div class="card shadow mb-4">
-    <div class="card-header py-3">
-        <h3 class="box-title">Form <?=$judul?></h3>
-        <div class="box-tools pull-right">
-            <a href="<?=base_url('member')?>" class="btn btn-sm btn-flat btn-warning">
-                <i class="fa fa-arrow-left"></i> Batal
-            </a>
-        </div>
+  <div class="card-header py-3">
+    <h3 class="box-title">Form <?= $judul ?></h3>
+    <div class="box-tools pull-right">
+      <a href="<?= base_url('member') ?>" class="btn btn-sm btn-flat btn-warning">
+        <i class="fa fa-arrow-left"></i> Batal
+      </a>
     </div>
-    <div class="card-body">
-        <div class="row justify-content-center">
-            <div class="col-sm-4 col-sm-offset-4">
-                <?=form_open('member/save', array('id'=>'member'), array('method'=>'add'))?>
-                    <div class="form-group">
-                        <label for="username">Username</label>
-                        <input autofocus="autofocus" onfocus="this.select()" placeholder="Username" type="text" name="username" class="form-control">
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group">
-                        <label for="nama">Nama Lengkap</label>
-                        <input placeholder="Nama" type="text" name="nama" class="form-control">
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email</label>
-                        <input placeholder="Email" type="email" name="email" class="form-control">
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group">
-                        <label for="jenis_kelamin">Jenis Kelamin</label>
-                        <select name="jenis_kelamin" class="form-control select2">
-                            <option value="">-- Pilih --</option>
-                            <option value="L">Laki-laki</option>
-                            <option value="P">Perempuan</option>
-                        </select>
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group">
-                        <label for="seleksi">Jurusan</label>
-                        <select id="seleksi" name="seleksi" class="form-control select2">
-                            <option value="" disabled selected>-- Pilih --</option>
-                        </select>
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group">
-                        <label for="kelas">Kelas</label>
-                        <select id="kelas" name="kelas" class="form-control select2">
-                            <option value="">-- Pilih --</option>
-                        </select>
-                        <small class="help-block"></small>
-                    </div>
-                    <div class="form-group float-right">
-                        <button type="reset" class="btn btn-flat btn-info"><i class="fa fa-rotate-left"></i> Reset</button>
-                        <button type="submit" id="submit" class="btn btn-flat btn-success"><i class="fa fa-save"></i> Simpan</button>
-                    </div>
-                <?=form_close()?>
-            </div>
+  </div>
+  <div class="card-body">
+    <div class="row justify-content-center">
+      <div class="col-sm-4 col-sm-offset-4">
+        <?= form_open('member/save', array('id' => 'member'), array('method' => 'add')) ?>
+        <div class="form-group">
+          <label for="username">Username</label>
+          <input value="aaaaaa" autofocus="autofocus" onfocus="this.select()" placeholder="Username" type="text" name="username" class="form-control">
+          <small class="help-block"></small>
         </div>
+        <div class="form-group">
+          <label for="nama">Nama Lengkap</label>
+          <input value="aaaaaa" placeholder="Nama" type="text" name="nama" class="form-control">
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group">
+          <label for="sekolah">Sekolah</label>
+          <input value="a" placeholder="SMA..." type="text" name="school" class="form-control">
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input value="a@a.a" placeholder="john@doe.com" type="email" name="email" class="form-control">
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input value="123456" placeholder="Password" type="password" name="password" class="form-control">
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group">
+          <label for="confirm_password">Konfirmasi Password</label>
+          <input value="123456" placeholder="Password" type="password" name="confirm_password" class="form-control">
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group">
+          <label for="jurusan">Jurusan</label>
+          <select name="jurusan" class="form-control select2">
+            <option value="" disabled>-- Pilih --</option>
+            <option value="0" selected>SAINTEK</option>
+            <option value="1">SOSHUM</option>
+          </select>
+          <small class="help-block"></small>
+        </div>
+        <div class="form-group float-right">
+          <button type="reset" class="btn btn-flat btn-info"><i class="fa fa-rotate-left"></i> Reset</button>
+          <button type="submit" id="submit" class="btn btn-flat btn-success"><i class="fa fa-save"></i> Simpan</button>
+        </div>
+        <?= form_close() ?>
+      </div>
     </div>
+  </div>
 </div>
 
-<script src="<?=base_url()?>assets/dist/js/app/master/member/add.js"></script>
+<script src="<?= base_url() ?>assets/dist/js/app/master/member/add.js"></script>
--- assets/dist/js/app/master/member/add.js
+++ assets/dist/js/app/master/member/add.js
@@ -1,93 +1,103 @@
 function load_seleksi() {
-    $('#seleksi').find('option').not(':first').remove();
+  $("#seleksi").find("option").not(":first").remove();
 
-    $.getJSON(base_url+'seleksi/load_seleksi', function (data) {
-        var option = [];
-        for (let i = 0; i < data.length; i++) {
-            option.push({
-                id: data[i].id_seleksi,
-                text: data[i].nama_seleksi
-            });
-        }
-        $('#seleksi').select2({
-            data: option
-        })
+  $.getJSON(base_url + "seleksi/load_seleksi", function (data) {
+    var option = [];
+    for (let i = 0; i < data.length; i++) {
+      option.push({
+        id: data[i].id_seleksi,
+        text: data[i].nama_seleksi,
+      });
+    }
+    $("#seleksi").select2({
+      data: option,
     });
+  });
 }
 
 function load_kelas(id) {
-    $('#kelas').find('option').not(':first').remove();
+  $("#kelas").find("option").not(":first").remove();
 
-    $.getJSON(base_url+'kelas/kelas_by_seleksi/' + id, function (data) {
-        var option = [];
-        for (let i = 0; i < data.length; i++) {
-            option.push({
-                id: data[i].id_kelas,
-                text: data[i].nama_kelas
-            });
-        }
-        $('#kelas').select2({
-            data: option
-        });
+  $.getJSON(base_url + "kelas/kelas_by_seleksi/" + id, function (data) {
+    var option = [];
+    for (let i = 0; i < data.length; i++) {
+      option.push({
+        id: data[i].id_kelas,
+        text: data[i].nama_kelas,
+      });
+    }
+    $("#kelas").select2({
+      data: option,
     });
+  });
 }
 
 $(document).ready(function () {
+  ajaxcsrf();
 
-    ajaxcsrf();
+  // Load Jurusan
+  // load_seleksi();
 
-    // Load Jurusan
-    load_seleksi();
+  // Load Kelas By Jurusan
+  $("#seleksi").on("change", function () {
+    load_kelas($(this).val());
+  });
 
-    // Load Kelas By Jurusan
-    $('#seleksi').on('change', function () {
-        load_kelas($(this).val());
-    });
+  $("form#member input, form#member select").on("change", function () {
+    $(this).closest(".form-group").removeClass("has-error has-success");
+    $(this).nextAll(".help-block").eq(0).text("");
+  });
 
-    $('form#member input, form#member select').on('change', function () {
-        $(this).closest('.form-group').removeClass('has-error has-success');
-        $(this).nextAll('.help-block').eq(0).text('');
-    });
+  $('[name="jenis_kelamin"]').on("change", function () {
+    $(this).parent().nextAll(".help-block").eq(0).text("");
+  });
 
-    $('[name="jenis_kelamin"]').on('change', function () {
-        $(this).parent().nextAll('.help-block').eq(0).text('');
-    });
+  $("form#member").on("submit", function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
 
-    $('form#member').on('submit', function (e) {
-        e.preventDefault();
-        e.stopImmediatePropagation();
+    var btn = $("#submit");
+    btn.attr("disabled", "disabled").text("Wait...");
 
-        var btn = $('#submit');
-        btn.attr('disabled', 'disabled').text('Wait...');
-
-        $.ajax({
-            url: $(this).attr('action'),
-            data: $(this).serialize(),
-            type: 'POST',
-            success: function (data) {
-                btn.removeAttr('disabled').text('Simpan');
-                if (data.status) {
-                    Swal({
-                        "title": "Sukses",
-                        "text": "Data Berhasil disimpan",
-                        "type": "success"
-                    }).then((result) => {
-                        if (result.value) {
-                            window.location.href = base_url+'member';
-                        }
-                    });
-                } else {
-                    console.log(data.errors);
-                    $.each(data.errors, function (key, value) {
-                        $('[name="' + key + '"]').nextAll('.help-block').eq(0).text(value);
-                        $('[name="' + key + '"]').closest('.form-group').addClass('has-error');
-                        if (value == '') {
-                            $('[name="' + key + '"]').nextAll('.help-block').eq(0).text('');
-                            $('[name="' + key + '"]').closest('.form-group').removeClass('has-error').addClass('has-success');
-                        }
-                    });
-                }
+    $.ajax({
+      url: $(this).attr("action"),
+      data: $(this).serialize(),
+      type: "POST",
+      success: function (data) {
+        btn.removeAttr("disabled").text("Simpan");
+        if (data.status) {
+          Swal({
+            title: "Sukses",
+            text: "Data Berhasil disimpan",
+            type: "success",
+          }).then((result) => {
+            if (result.value) {
+              window.location.href = base_url + "member";
             }
-        });
+          });
+        } else {
+          console.log(data.errors);
+          $.each(data.errors, function (key, value) {
+            $('[name="' + key + '"]')
+              .nextAll(".help-block")
+              .eq(0)
+              .text(value);
+            $('[name="' + key + '"]')
+              .closest(".form-group")
+              .addClass("has-error");
+            if (value == "") {
+              $('[name="' + key + '"]')
+                .nextAll(".help-block")
+                .eq(0)
+                .text("");
+              $('[name="' + key + '"]')
+                .closest(".form-group")
+                .removeClass("has-error")
+                .addClass("has-success");
+            }
+          });
+        }
+      },
     });
+  });
 });
